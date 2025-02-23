d4ef99c4dfe3890ec43718a26640e36b8d5d47c5
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2025-02-23 16:35:31 -0300

    debug

--- server.js
+++ server.js
@@ -290,6 +290,7 @@ app.post('/register', async (req, res) => {
         });
         res.status(201).json({ message: 'Usuário criado com sucesso!' });
     } catch (error) {
+        console.log(error.message)
         res.status(500).json({ message: 'Erro ao criar usuário', error: error.message });
     }
 });
